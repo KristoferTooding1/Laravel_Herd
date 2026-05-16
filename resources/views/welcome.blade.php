562667caf7f8877e0d7d0cb16789d0bd5613ba69
KristoferTooding1 <tooding2006@gmail.com>
2026-05-16 14:35:29 +0300

    3rd Episode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<x-layout>
     <h1>Hello, World!</h1>
-    <div>
-        <a href="/about">About us</a>
-    </div>
-</body>
-</html>
+</x-layout>
