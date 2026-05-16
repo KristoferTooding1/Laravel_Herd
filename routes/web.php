7ae59802f8dfcb445fcb89090e2f2d898fe5affe
KristoferTooding1 <tooding2006@gmail.com>
2026-05-16 15:25:58 +0300

    4th Episode

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-Route::view('/about', 'about');
-Route::view('/contact', 'contact');
+Route::get('/', function () {
+    return view('welcome', [
+        'greeting' => 'Hello',
+        'person' => request('person', 'World'),
+    ]);
+});
 
